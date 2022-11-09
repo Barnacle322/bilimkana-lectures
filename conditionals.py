614641eda3f820984a7b2c2deb9b7ea8a9e02d2c
Bilimkana <arstan.usenov@gmail.com>
2022-11-09 10:35:01 +0600

    First period

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+# Условные выражения
+a = 5
+b = 7
+c = 8
+
+if a > b and c > 7: # a > b & c > 7
+    print("Here")
+
+elif not (c > 4 or c < 20): # c > 4 | True
+    print("I'm here")
+
+elif a == b:
+    print("A is equal to B")
+
+else:
+    print("No, here")
+
+# Logic Gates
+# and &
+# or |
+# not !
---
+++ loops.py
@@ -0,0 +1,7 @@
+# Циклы
+a = 0
+
+
+while a < 25000:
+    print(str(a) + " HI")
+    a = a + 1
