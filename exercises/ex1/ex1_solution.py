f264acd74300cc7c9ce665b275d12d557264a8b1
Arstanbek Usenov <arstan.usenov@gmail.com>
2022-11-23 19:40:00 +0600

    Added shortened solutions

--- exercises/ex1/ex1_solution.py
+++ exercises/ex1/ex1_solution.py
@@ -13,3 +13,8 @@ def calculator(a: int, b: int) -> int:
 
 print(calculator(30, 40))  # -> 70
 print(calculator(30, 20))  # -> 600
+
+# Еще короче решение
+func = lambda a, b: a + b if a * b >= 1000 else a * b
+print(func(30, 40))  # -> 70
+print(func(30, 20))  # -> 600
--- exercises/ex4/ex4_solution.py
+++ exercises/ex4/ex4_solution.py
@@ -18,3 +18,11 @@ def add_to_dict(key, value, dictionary):
 my_dict = {"Apple": "iPhone", "Samsung": "Galaxy"}
 add_to_dict("Xiaomi", "Mi", my_dict)
 add_to_dict("Xiaomi", "Something else", my_dict)
+
+
+# Еще короче
+func = lambda key, value, dictionary: dictionary.setdefault(key, value)
+my_dict = {"Apple": "iPhone", "Samsung": "Galaxy"}
+func("Xiaomi", "Mi", my_dict)
+func("Xiaomi", "Something else", my_dict)
+    
