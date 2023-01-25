50bf6110d1cf5374c4fb4949b7a228cf501c9281
Arstanbek Usenov <arstan.usenov@gmail.com>
2023-01-25 11:49:46 +0600

    🗃️ Final DB definition

--- webapp/app.py
+++ webapp/app.py
@@ -10,6 +10,8 @@ db = SQLAlchemy(app)
 
 
 class Student(db.Model):
+    __tablename__ = "student"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=True)
