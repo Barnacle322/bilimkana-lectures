7c1d379da245d198dbb1cce79876d1c9b49fef71
Arstanbek Usenov <arstan.usenov@gmail.com>
2023-01-25 10:54:06 +0600

    🗃️ Removed models file

--- webapp/app.py
+++ webapp/app.py
@@ -4,25 +4,30 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///students.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False      
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
 
-userlist = []
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    age = db.Column(db.Integer, nullable=True)
+    grade = db.Column(db.String(3), nullable=False)
 
 
 @app.get("/add/<name>")
 def index(name: str):
-    userlist.append(name.capitalize())
+    student = Student(name=name, age=18, grade="11A")
+
+    db.session.add(student)
+    db.session.commit()
     return "Added"
 
 
 @app.get("/userlist")
 def users():
-    return render_template("users.html", userlist=userlist)
+    return render_template("users.html")
 
 
 if __name__ == "__main__":
--- webapp/models.py
+++ webapp/models.py
@@ -1,8 +1,3 @@
 from app import db
 
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    age = db.Column(db.Integer, nullable=True)
-    grade = db.Column(db.String(3), nullable=False)
